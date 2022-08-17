928f742d4417bd578036fbf2787cb38f685f3609
Ram4GB <leminhcuong2988@yahoo.com>
2022-08-17 16:31:51 +0700

    done

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
-const rootDir = resolve(__dirname, 'src/pages');
-const buildDir = resolve(__dirname, 'dist')
+// const rootDir = resolve(__dirname, 'src/pages');
+// const buildDir = resolve(__dirname, 'dist')
 
 export default defineConfig(() => {
     return {
         root: process.cwd(),
-        // build: {
-        //     rollupOptions: {
-        //         input: {
-        //             index: resolve(rootDir, 'index.html'),
-        //         },
-        //     },
-        //     outDir: buildDir
-        // },
+        build: {
+            rollupOptions: {
+                input: {
+                    main: resolve(__dirname, 'index.html'),
+                    nested1: resolve(__dirname, 'src/pages/index.html'),
+                    nested2: resolve(__dirname, 'nested/index.html'),
+                },
+            },
+        },
     }
 })
